3e79fcfdd9728bb09421f305347a22bb62fcdad0
congcong <740707701@qq.com>
2016-06-13 19:44:47 +0800

    fabric.js

--- script/dragImg.js
+++ script/dragImg.js
@@ -12,12 +12,43 @@
 	var circle = new fabric.Circle({
 		radius:25,left:50,top:200,fill:"blue"
 	});
-	//circle.set("globalCompositeOperation","source-over"); //重叠属性
+	//circle.set("globalCompositeOperation","destination-over"); //重叠属性
 	var triangle = new fabric.Triangle({
 		width:100,height:100,left:50,top:200,fill:"purple"
 	});
 	var img = new fabric.Image.fromURL("images/dragImg/01.jpg",function(img){
-		canvas.add(img.set({left:200,top:100,angle:45}));
+		canvas.add(img.set({left:200,top:100,angle:45,globalCompositeOperation:"destination-over"}));
 	});
-	canvas.add(rect1,rect2,rect3,circle,triangle);
+	var img2 = new fabric.Image.fromURL("images/dragImg/02.jpg",function(img){
+		canvas.add(img.set({left:150,top:100,angle:45,globalCompositeOperation:"destination-over"}));
+	});
+	/*var img3 = new fabric.Image.fromURL("images/dragImg/03.jpg",function(img){
+		canvas.add(img.set({left:220,top:100,angle:45,globalCompositeOperation:"destination-over"}));
+	});
+	var img4 = new fabric.Image.fromURL("images/dragImg/04.jpg",function(img){
+		canvas.add(img.set({left:280,top:100,angle:45,globalCompositeOperation:"destination-over"}));
+	});*/
+
+	canvas.add(rect1,rect2,rect3,circle,triangle);//globalCompositeOperation
+
+	canvas.on({
+	   'object:moving': onChange,
+	    'object:scaling': onChange,
+	   'object:rotating': onChange,
+	   'object:clicking':onChange,
+	});
+
+  function onChange(options) {
+    options.target.setCoords();//坐标
+    canvas.forEachObject(function(obj) {
+      if (obj === options.target) return;
+      obj.setOpacity(options.target.intersectsWithObject(obj) ? 1 : 1);
+    	obj.set({"Canvas.ZIndex":9999,borderColor:"red"});
+    	obj["Canvas.ZIndex"] = 999
+  		//obj.set({"globalCompositeOperation":"destination-over"});
+    	obj.hasControls=false;
+
+       canvas.renderAll.bind(canvas); //this.__canvases.push(canvas);
+    });
+  }
 })();
