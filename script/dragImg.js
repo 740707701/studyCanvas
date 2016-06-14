9e0f9846b84f46d56dbc836dbe995c1c920e2974
congcong <740707701@qq.com>
2016-06-14 19:30:32 +0800

    text fabric.js

--- script/dragImg.js
+++ script/dragImg.js
@@ -60,5 +60,67 @@
         //canvas.renderAll.bind(canvas); 
        	// this.__canvases.push(canvas);
     });
-  }
+  };
+
+  //文字
+  var text = new fabric.Text("这是第一段文字",{
+  	fontFamily:"Arial",
+  	left:200,
+  	top:500,
+  	fontSize:40,
+  	textAlign:"left",
+  	fill:"green",
+  });
+  canvas.add(text);
+
+  //可编辑文字
+  var textarea = new fabric.IText("this is textarea\nincluding fontFamily \nor fontSize ",{
+  	left:100,
+  	top:500,
+  	fontFamily:"Courier", 
+  	fontSize:20,
+  	fill:"#333",
+  	styles: {
+  		0:{
+  			0: {fontSize:50,textBackgroundColor:"green",fill:"yellow",textDecoration:"line-through"},
+  			3: {fontSize:50,fill:"red"},
+  			10: {fontSize:80,fontWeight:"bold",stroke:"lightgreen",strokeWidth:3,shadow:"rgba(0,0,0,0.3) 5px 2px 2px"}
+  		},
+  		1: {
+  			0:{fontSize:50,fill:"red",shadow:"rgba(0,255,0,0.5) 3px 2px 2px"},
+  			5:{fontSize:40,fontStyle:"italic"}
+  		}
+  	}
+  });
+  var textarea2 = new fabric.IText("xxxx",{
+  	left:500,
+  	top:600,
+  	fontFamily:"Courier",
+  	fontSize:80,
+  	padding:0,
+  	angle:20,
+  	lingHeight:1,
+  	styles: {
+  		0: {
+  			0: {textBackgroundColor:"#555"},
+  			1: {textBackgroundColor:"#777"},
+  			2: {textBackgroundColor:"#999"},
+  			3: {textBackgroundColor:"#ddd"}
+  		}
+  	}
+  });
+  canvas.add(textarea,textarea2);
+
+//var canvasUrl = canvas.toDataUrl("image/png");
+
+
 })();
+var canvas = this._canvas = new fabric.Canvas("canvas");
+var down = function(){
+	var dataUrl = canvas.toDataURL({
+		format:"png",
+		multiplier:2
+	});
+	var a = document.getElementById("a");
+	a.href = dataUrl;
+};
