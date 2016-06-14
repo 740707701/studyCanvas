e1f3965f7043cc999b9eb8571417d9056d73ef06
congcong <740707701@qq.com>
2016-06-14 11:13:06 +0800

    dragImg 实现图片任意拖动 旋转 拉伸 finish

--- script/dragImg.js
+++ script/dragImg.js
@@ -12,43 +12,53 @@
 	var circle = new fabric.Circle({
 		radius:25,left:50,top:200,fill:"blue"
 	});
-	//circle.set("globalCompositeOperation","destination-over"); //重叠属性
 	var triangle = new fabric.Triangle({
 		width:100,height:100,left:50,top:200,fill:"purple"
 	});
 	var img = new fabric.Image.fromURL("images/dragImg/01.jpg",function(img){
-		canvas.add(img.set({left:200,top:100,angle:45,globalCompositeOperation:"destination-over"}));
+		canvas.add(img.set({left:200,top:100,angle:45}));
 	});
 	var img2 = new fabric.Image.fromURL("images/dragImg/02.jpg",function(img){
-		canvas.add(img.set({left:150,top:100,angle:45,globalCompositeOperation:"destination-over"}));
+		canvas.add(img.set({left:150,top:100,angle:45}));
 	});
-	/*var img3 = new fabric.Image.fromURL("images/dragImg/03.jpg",function(img){
-		canvas.add(img.set({left:220,top:100,angle:45,globalCompositeOperation:"destination-over"}));
+	var img3 = new fabric.Image.fromURL("images/dragImg/03.jpg",function(img){
+		canvas.add(img.set({left:220,top:100,angle:45}));
 	});
 	var img4 = new fabric.Image.fromURL("images/dragImg/04.jpg",function(img){
-		canvas.add(img.set({left:280,top:100,angle:45,globalCompositeOperation:"destination-over"}));
-	});*/
+		canvas.add(img.set({left:280,top:100,angle:45}));
+	});
 
 	canvas.add(rect1,rect2,rect3,circle,triangle);//globalCompositeOperation
 
+	/*
+		canvas.on("object:selected",function(options){
+			options.target.bringToFront(); //rect circle,triangle,层叠顺序
+		});
+	*/
+   	/*
+		四个命令可以改变叠加顺序:
+		options.target.sendBackwards()
+		options.target.sendToBack()
+		options.target.bringForward()
+		options.target.bringToFront()
+   	*/
 	canvas.on({
 	   'object:moving': onChange,
-	    'object:scaling': onChange,
+	   'object:scaling': onChange,
 	   'object:rotating': onChange,
-	   'object:clicking':onChange,
+	   "object:selected":onChange,//选中事件
 	});
 
   function onChange(options) {
     options.target.setCoords();//坐标
+   	options.target.bringToFront(); //层叠顺序
     canvas.forEachObject(function(obj) {
-      if (obj === options.target) return;
-      obj.setOpacity(options.target.intersectsWithObject(obj) ? 1 : 1);
-    	obj.set({"Canvas.ZIndex":9999,borderColor:"red"});
-    	obj["Canvas.ZIndex"] = 999
-  		//obj.set({"globalCompositeOperation":"destination-over"});
-    	obj.hasControls=false;
-
-       canvas.renderAll.bind(canvas); //this.__canvases.push(canvas);
+      	if (obj === options.target) return;
+     	//obj.setOpacity(options.target.intersectsWithObject(obj) ? 1 : 1); //透明度
+   		obj.hasControls=false; //没有边框
+   		obj.hasBorders = false; //没有四边
+        //canvas.renderAll.bind(canvas); 
+       	// this.__canvases.push(canvas);
     });
   }
 })();
